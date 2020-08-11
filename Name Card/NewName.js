94b29599a54e2dae17935ffe3265e38ffd0bb371
daydaddy <jn_day@hotmail.com>
2020-08-11 08:45:31 -0600

    added namecard assignment

---
+++ Name Card/NewName.js
@@ -0,0 +1,5 @@
+function changeName(){
+    let inputName = document.getElementById("username")
+    let badgeName = document.getElementById("badgeName")
+    badgeName.innerHTML = inputName.value
+}
