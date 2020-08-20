24496a6ff344e1dc051e74d891efc1992e08569c
daydaddy <jn_day@hotmail.com>
2020-08-20 13:31:06 -0600

    added frontEnd.ejs

---
+++ frontendEJS/routes/food.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs')
+const path = require('path')
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+let foodItems = {}
+    
+try {
+  
+  const data = fs.readFileSync(path.join(__dirname,'../data/food.json'), 'utf8')
+  foods = JSON.parse(data)
+
+} catch (err) {
+  console.error(err)
+}
+
+  res.render('food', foods);
+});
+
+module.exports = router;
